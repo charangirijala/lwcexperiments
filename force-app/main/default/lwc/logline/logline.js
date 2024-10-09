1e2723d14c80fffd880598aa9cd7087b759c1b2b
Akashreddy19 <girijalac@gmail.com>
2024-10-09 21:08:22 +0530

    changes to logline

--- force-app/main/default/lwc/logline/logline.js
+++ force-app/main/default/lwc/logline/logline.js
@@ -1,26 +1,48 @@
 import { LightningElement, api } from "lwc";
+import { loadStyle } from "lightning/platformResourceLoader";
+import customTableStyling from "@salesforce/resourceUrl/customTableStyling";
 
 export default class Logline extends LightningElement {
   @api logLine;
+  eventClassComb = "slds-line-clamp";
 
   get VARIN() {
     if (this.logLine.logLineData) {
-      if (this.logLine.logLineData.type === "VARIN") return true;
+      if (this.logLine.logLineData.type === "VARIN") {
+        this.eventClassComb += " varin";
+        return true;
+      }
     }
     return false;
   }
 
   get VARAS() {
     if (this.logLine.logLineData) {
-      if (this.logLine.logLineData.type === "VARAS") return true;
+      if (this.logLine.logLineData.type === "VARAS") {
+        this.eventClassComb += " varas";
+        return true;
+      }
     }
     return false;
   }
 
   get DEBUG() {
     if (this.logLine.logLineData) {
-      if (this.logLine.logLineData.type === "DEBUG") return true;
+      if (this.logLine.logLineData.type === "DEBUG") {
+        this.eventClassComb += " debug";
+        return true;
+      }
     }
     return false;
+  }
+
+  get CODEUNIT() {
+    if (this.logLine.type === "codeunit") {
+    }
+  }
+
+  get METHODUNIT() {
+    if (this.logLine.type === "methodunit") {
+    }
   }
 }
